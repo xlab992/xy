a84fa7c0cfab5ea1469a16c2ee9834a8e88cc8d8
ciccioxm3 <cicciariello@proton.me>
2025-08-04 17:48:02 +0200

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -359,7 +359,7 @@ def save_m3u8(channels):
         os.remove(OUTPUT_FILE)
 
     with open(OUTPUT_FILE, "w", encoding="utf-8") as f:
-        f.write(f'#EXTM3U url-tvg="https://git.pizzapi.uk/{NOMEGITHUB}/{NOMEREPO}/raw/branch/main/epg.xml"\n\n')
+        f.write(f'#EXTM3U url-tvg="https://github.com/{NOMEGITHUB}/{NOMEREPO}/raw/branch/main/epg.xml"\n\n')
         user_agent = extract_user_agent()
 
         for name, url, category in channels:
